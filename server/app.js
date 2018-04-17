68b5db50446108d6271d4fc58c1fdec781a7ef8a
eminoda <245978782@qq.com>
2018-04-17 23:56:05 +0800

    plan

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
+const serve = require('koa-static');
 const koaNunjucks = require('koa-nunjucks-2');
 const path = require('path');
 // router
 const blogRouter = require('./router/blogRouter');
+const indexRouter = require('./router/indexRouter');
 
 // middleware
+app.use(serve(__dirname + '/public'));
 app.use(koaNunjucks({
     ext: 'html',
     path: path.join(__dirname, 'views'),
@@ -13,8 +16,9 @@ app.use(koaNunjucks({
         trimBlocks: true
     }
 }));
-app.use(blogRouter.routes())
-    .use(blogRouter.allowedMethods());
+// router register
+app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
+app.use(blogRouter.routes()).use(blogRouter.allowedMethods());
 
 app.use(async ctx => {
     ctx.body = 'Hello World';
