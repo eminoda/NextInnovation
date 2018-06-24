3c72644fd610ac7a3f45681d897968a24db9b176
eminoda <245978782@qq.com>
2018-06-24 19:43:44 +0800

    confict

--- server/router/oauthRouter.js
+++ server/router/oauthRouter.js
@@ -4,10 +4,7 @@ const logger = require('../service/loggerService')('oauthRouter');
 const githubConf = require('../conf/github.conf');
 const userService = require('../service/userService');
 const oauthService = require('../service/oauthService');
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
-=======
->>>>>>> b65e33db7fe607488d91bb6d0f75e9c59fb5d8dd
 router.prefix('/oauth');
 
 // 开始授权
@@ -16,15 +13,10 @@ router.get('/', async (ctx, next) => {
     let redirect_uri = githubConf.redirect_uri;
     let state = 123456; //TODO 加密
     // 通过github，重定向到指定路径
-<<<<<<< HEAD
     console.log(ctx.query.backUrl || ctx.query.backurl);
     let third_redirect_uri = '?backUrl=' + githubConf.blog_uri + jwt.decode(decodeURIComponent(ctx.query.backUrl || ctx.query.backurl), 'hexo');
     console.log(third_redirect_uri);
     ctx.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&state=${state}&redirect_uri=${redirect_uri}${third_redirect_uri}`);
-=======
-    let third_redirect_uri = '?backUrl=http://www.baidu.com';
-    ctx.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&state=${state}&redirect_uri=${redirect_uri}` + third_redirect_uri);
->>>>>>> b65e33db7fe607488d91bb6d0f75e9c59fb5d8dd
 })
 
 // 
